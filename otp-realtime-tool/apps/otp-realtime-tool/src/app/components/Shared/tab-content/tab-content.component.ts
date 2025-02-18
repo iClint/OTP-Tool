34f13f07ddc9f46dbcf129e114c146fb4dfe35b2
Clint Kingston <ckingston1@woolworths.com.au>
2025-02-18 16:44:19 +1100

    file structure

--- otp-realtime-tool/apps/otp-realtime-tool/src/app/components/Shared/tab-content/tab-content.component.ts
+++ otp-realtime-tool/apps/otp-realtime-tool/src/app/components/Shared/tab-content/tab-content.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { PresetMessagesComponent } from '../preset-messages/preset-messages.component';
-import { CustomJsonComponent } from '../custom-json/custom-json/custom-json.component';
+import { CustomJsonComponent } from '../custom-json/custom-json.component';
 import { OtpPreviewComponent } from '../otp-preview/otp-preview.component';
 import { TabContent } from '../../../models/config-app.model';
 import { SendMapUpdateComponent } from '../send-map-update/send-map-update.component';
